2d9b1da26559148caca02e60dee7d67fba4ad2fd
kiran <kkchaulagain@gmail.com>
2023-07-03 11:29:58 +0545

    work

--- raspberry_pi_camera_streaming.py
+++ raspberry_pi_camera_streaming.py
@@ -119,7 +119,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='1280x720', framerate=30) as camera:
+with picamera.PiCamera(resolution='1280x720', framerate=60) as camera:
     output = StreamingOutput()
     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
