07cfb7a8f618e694e121f2680551be75467cab7b
kiran <kkchaulagain@gmail.com>
2023-07-03 11:32:52 +0545

    change in dimesnio

--- raspberry_pi_camera_streaming.py
+++ raspberry_pi_camera_streaming.py
@@ -1,6 +1,3 @@
-# Source code from the official PiCamera package
-# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
-
 import io
 import picamera
 import logging
@@ -12,49 +9,62 @@ PAGE="""\
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Raspberry Pi Camera | Surveillance</title>
-	<meta name="keywords" content="Live footage from Raspberry Pi">
-	<meta name="description" content="Surveillance">
-	<meta name="format-detection" content="telephone=no">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="shortcut icon" href="https://mastersubhajit.com/assets/favicon.ico">
-	<link rel="stylesheet" href="https://demo.artureanec.com/html/quadron/css/style.css">
-	<!-- Load google font
-	================================================== -->
-	<script>
-		WebFontConfig = {
-			google: { families: [ 'Roboto:300,400,400i,500,700', 'Montserrat:700'] }
-		};
-		(function() {
-			var wf = document.createElement('script');
-			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-			wf.type = 'text/javascript';
-			wf.async = 'true';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(wf, s);
-		})();
-	</script>
+    <meta charset="utf-8">
+    <title>Raspberry Pi Camera | Surveillance</title>
+    <meta name="keywords" content="Live footage from Raspberry Pi">
+    <meta name="description" content="Surveillance">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="https://mastersubhajit.com/assets/favicon.ico">
+    <link rel="stylesheet" href="https://demo.artureanec.com/html/quadron/css/style.css">
+    <!-- Load google font
+    ================================================== -->
+    <script>
+        WebFontConfig = {
+            google: { families: [ 'Roboto:300,400,400i,500,700', 'Montserrat:700'] }
+        };
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })();
+    </script>
 </head>
 <body>
 <main id="mainContent">
-	<div class="layout404">
-		<div class="layout404__logo"><a href="/">Surveillance</a></div>
-		<div class="container">
-			<div class="row">
-				<div class="layout404__img">
-					<img src="https://mastersubhajit.com/pi-camera.png" alt="">
-				</div>
-				<div class="col-lg-8 offset-lg-2">
-						<center><img src="stream.mjpg" class="img-fluid rounded" width="1280" height="720"></center>
-					<a href="/" class="btn-border btn-color-01">Refresh</a>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="layout404">
+        <div class="layout404__logo"><a href="/">Surveillance</a></div>
+        <div class="container">
+            <div class="row">
+                <div class="layout404__img">
+                    <img id="live-image" src="stream.mjpg" alt="">
+                </div>
+                <div class="col-lg-8 offset-lg-2">
+                    <button id="capture-btn" onclick="captureImage()">Capture</button>
+                    <div id="captured-image"></div>
+                    <a href="/" class="btn-border btn-color-01">Refresh</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://demo.artureanec.com/html/quadron/js/bundle.js"></script>
+<script>
+    function captureImage() {
+        var img = document.getElementById('live-image');
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+        var imageDataUrl = canvas.toDataURL('image/jpeg');
+        document.getElementById('captured-image').innerHTML = '<img src="' + imageDataUrl + '" alt="Captured Image">';
+    }
+</script>
 </body>
 </html>
 """
@@ -89,7 +99,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
-        elif self.path == '/stream.mjpg' or self.path == '/streaming':
+        elif self.path == '/stream.mjpg':
             self.send_response(200)
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
@@ -119,10 +129,8 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='1280x720', framerate=60) as camera:
+with picamera.PiCamera(resolution='1280x720', framerate=30) as camera:
     output = StreamingOutput()
-    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    #camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
     try:
         address = ('', 8000)
